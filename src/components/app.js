046b69e710a1248e51307689882b690f15a4fa5d
Krzysztof Rudowski <krzysztof.rudowski@schibsted.pl>
2017-06-11 13:18:18 +0200

    remove Router history init

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
 import Layout from './layout/layout';
 
-const customHistory = createBrowserHistory();
-
 const App = () => (
-  <Router history={customHistory}>
+  <Router>
     <Layout />
   </Router>
 );
