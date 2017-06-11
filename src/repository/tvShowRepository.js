ba114d7d4e8094861ebd5d74578de3d3cbde14ff
Krzysztof Rudowski <krzysztof.rudowski@schibsted.pl>
2017-06-11 13:34:09 +0200

    extract fetching json

--- src/repository/tvShowRepository.js
+++ src/repository/tvShowRepository.js
@@ -3,16 +3,11 @@ import transformer from '../transformer/tvShowTransformer';
 
 const API_BASE_URL = 'http://api.tvmaze.com';
 
+const fetchJson = url => fetch(url).then(res => res.json());
+
 const tvShowRepository = {
-  search: query =>
-     fetch(`${API_BASE_URL}/search/shows?q=${query}`)
-      .then(res => res.json())
-      .then(transformer.transformCollection),
-  findById: id =>
-     fetch(`${API_BASE_URL}/shows/${id}`)
-      .then(res => res.json())
-      .then(transformer.transformItem)
-  ,
+  search: query => fetchJson(`${API_BASE_URL}/search/shows?q=${query}`).then(transformer.transformCollection),
+  findById: id => fetchJson(`${API_BASE_URL}/shows/${id}`).then(transformer.transformItem),
 };
 
 export default tvShowRepository;
